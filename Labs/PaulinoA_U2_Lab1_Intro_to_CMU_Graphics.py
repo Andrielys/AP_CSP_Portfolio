7b369bd5dab647c561f978f688afe62257f086f6
Andrielys <ANdrielysp@nycstudents.net>
2024-10-25 18:00:23 -0400

    Add files via upload

---
+++ Labs/PaulinoA_U2_Lab1_Intro_to_CMU_Graphics.py
@@ -0,0 +1,14 @@
+# Import the CMU Graphics package:
+from cmu_graphics import *
+
+# Wide thick rectangle 
+Rect(50,50,160,100)
+# Smaller thick rectangle
+Rect(250,60,100,80)
+# Thicker Square
+Rect(190,190,90,80)
+# Smaller wider rectangle 
+Rect(150,300,90,40)
+
+# Run program:
+cmu_graphics.run()
