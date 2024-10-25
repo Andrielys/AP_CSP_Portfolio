cc746331f4e68c4be4f7bd90a3e6643cd1eaf777
Andrielys <ANdrielysp@nycstudents.net>
2024-10-25 19:45:51 -0400

    Add files via upload

---
+++ Labs/LastNameFirstInitial_U2_Lab4_Ovals_Circles_Stars.py
@@ -0,0 +1,12 @@
+# Import the CMU Graphics package:
+from cmu_graphics import *
+
+
+Oval(200,200,300,370, fill=rgb(255, 160, 122))
+Circle(200,200,100, fill='coral')
+Star(200,200,50,5, fill=gradient('cornSilk','gold', start='center'), align='center')
+
+
+
+# Run program:
+cmu_graphics.run()
